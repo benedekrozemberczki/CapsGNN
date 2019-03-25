d95b865b4143be9ee8a5edbf9c1a53b2b9436d42
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-03-25 10:33:38 +0000

    Update layers.py

--- src/layers.py
+++ src/layers.py
@@ -38,8 +38,6 @@ class ListModule(torch.nn.Module):
         """
         return len(self._modules)
 
-
-
 class PrimaryCapsuleLayer(torch.nn.Module):
     def __init__(self, in_units, in_channels, num_units, capsule_dimensions):
         super(PrimaryCapsuleLayer, self).__init__()
@@ -84,45 +82,20 @@ class SecondaryCapsuleLayer(torch.nn.Module):
 
     def forward(self, x):
         batch_size = x.size(0)
-
-        # (batch, in_units, features) -> (batch, features, in_units)
         x = x.transpose(1, 2)
-
-        # (batch, features, in_units) -> (batch, features, num_units, in_units, 1)
         x = torch.stack([x] * self.num_units, dim=2).unsqueeze(4)
-
-        # (batch, features, in_units, unit_size, num_units)
         W = torch.cat([self.W] * batch_size, dim=0)
-
-        # Transform inputs by weight matrix.
-        # (batch_size, features, num_units, unit_size, 1)
         u_hat = torch.matmul(W, x)
-
-        # Initialize routing logits to zero.
         b_ij = Variable(torch.zeros(1, self.in_channels, self.num_units, 1))
 
-        # Iterative routing.
         num_iterations = 3
         for iteration in range(num_iterations):
-            # Convert routing logits to softmax.
-            # (batch, features, num_units, 1, 1)
             c_ij = torch.nn.functional.softmax(b_ij,dim=0)
             c_ij = torch.cat([c_ij] * batch_size, dim=0).unsqueeze(4)
-
-            # Apply routing (c_ij) to weighted inputs (u_hat).
-            # (batch_size, 1, num_units, unit_size, 1)
             s_j = (c_ij * u_hat).sum(dim=1, keepdim=True)
-
-            # (batch_size, 1, num_units, unit_size, 1)
             v_j = SecondaryCapsuleLayer.squash(s_j)
-
-            # (batch_size, features, num_units, unit_size, 1)
             v_j1 = torch.cat([v_j] * self.in_channels, dim=1)
-
-            # (1, features, num_units, 1)
             u_vj1 = torch.matmul(u_hat.transpose(3, 4), v_j1).squeeze(4).mean(dim=0, keepdim=True)
-
-            # Update b_ij (routing)
             b_ij = b_ij + u_vj1
 
         return v_j.squeeze(1)
