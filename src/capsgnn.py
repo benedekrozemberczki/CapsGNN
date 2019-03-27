87a8ca95ab6880c9933bc2faa4f835e0a7b2de10
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-03-27 15:32:26 +0000

    Update capsgnn.py

--- src/capsgnn.py
+++ src/capsgnn.py
@@ -33,7 +33,7 @@ class CapsGNN(torch.nn.Module):
         2. Primary capsules.
         3. Attention
         4. Graph capsules.
-        4. Class capsules.
+        5. Class capsules.
         """
         self.base_layers = [GCNConv(self.number_of_features, self.args.gcn_filters)]
         for layer in range(self.args.gcn_layers-1):
